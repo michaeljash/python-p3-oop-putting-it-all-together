66ddc81a4c078c06a6576b287dfa4b3a9436948a
jashonmichael <jashon.michael@student.moringaschool.com>
2024-05-24 12:25:29 +0300

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, author, pages):
+
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def pick1(self):
+        print ("Picked book1")
+    def pick2(self):
+        print("Picked book2")
+    def pick3(self):
+        print("Picked book3")
+    def pick4(self):
+        print("Picked book4")
+
+book1 = Book("Death Row","Michael.B","642")
+book2 = Book("Atlas","Jeniffer.L","579")
+book3 = Book("World of Apes","Jason","945")
+book4 = Book("Debugging","Michael Jashon","756")
+
+
+book1.pick1()
+book2.pick2()
+book3.pick3()
+book4.pick4()
+
+print(book1)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    brand = "Loui.V"
+    def __init__(self,name,size,color):
+        self.name = name
+        self.size = size
+        self.color = color
+
+    def pick_it(self):
+        print("Picked shoe")
+    def remove_it(self):
+        print("Removed it")
+
+shoe1 = Shoe("Avunja","42","pitch")
+shoe1.pick_it()
+shoe2 = Shoe("Prada","32","green")
+shoe2.pick_it()
+shoe3 = Shoe("Puma","30","blue")
+shoe1.remove_it()
+shoe4 = Shoe("Air max","37","black")
+shoe1.remove_it()
+
